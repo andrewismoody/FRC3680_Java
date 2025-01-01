00f1cae6fd7d108f70189eb94ca15449077db684
ammoody <andrewismoody@gmail.com>
2025-01-01 10:32:22 -0500

    Initial Auto Interface Setup
    Creates Auto interface for extending future auto routines
    Need to better integrate with module controller here - need a way to
    select modules with an enum or indexer so we don't have to pass every
    module individually to the Auto class.

--- src/main/java/frc/robot/RobotModule.java
+++ src/main/java/frc/robot/RobotModule.java
@@ -1,6 +1,6 @@
 package frc.robot;
 
-interface RobotModule {
+public interface RobotModule {
     public void Initialize();
     public void ProcessState(boolean value);
     public void SetController(ModuleController controller);
